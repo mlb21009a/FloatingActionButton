e27679c1be5514f44d418dff029a78399d87a3cf
mlb21009a <mlb21009a@gmail.com>
2017-04-26 21:50:52 +0900

    Initial Commit

---
+++ FloatingActionButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FloatingActionButton
+//
+//  Created by R on 2017/04/26.
+//  Copyright © 2017年 マック太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
